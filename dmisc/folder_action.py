615e77a503e57642edc46d5b9034bc34ec4377f7
Douglas Wu <wckdouglas@gmail.com>
2016-04-16 01:42:49 -0500

    update Sat Apr 16 01:42:49 CDT 2016

--- dmisc/folder_action.py
+++ dmisc/folder_action.py
@@ -3,8 +3,8 @@ import sys
 
 def makeFolder(folder):
     if not os.path.isdir(folder):
-        sys.stderr.write('Created %s' %folder)
+        sys.stderr.write('Created %s\n' %folder)
         os.mkdir(folder)
     else:
-        sys.stderr.write('%s exists!' %folder)
+        sys.stderr.write('%s exists!\n' %folder)
     return 0
